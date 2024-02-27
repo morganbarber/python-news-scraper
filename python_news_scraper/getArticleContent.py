b35b0d3f140dc8e1728eebd4b96a805682af8353
Morgan Barber <109038897+morganbarber@users.noreply.github.com>
2024-02-27 18:58:59 +0000

    Add headers to requests in getArticleContent.py

--- python_news_scraper/getArticleContent.py
+++ python_news_scraper/getArticleContent.py
@@ -16,8 +16,15 @@ def getArticleContent(articles, filterWords):
     return processedArticles
 
 def extractArticleContentAndFavicon(article, filterWords):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+    }
     try:
-        response = requests.get(article['url'])
+        response = requests.get(article['url'], headers=headers, timeout=1)
         if response.status_code == 200:
             print("response success")
             content = response.text
